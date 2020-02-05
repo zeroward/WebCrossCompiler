a0274054aa25c2381709d2ffedcbc0755b97b7d5
zeroward <zeroward@outlook.com>
2020-02-04 18:20:06 -0600

    Fixed url issue

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def upload_file():
                 return send_file(outfilepath, as_attachment=True)
             except:
                 flash("Compilation Failed, Check File and Try again")
-                return redirect(request.uri)
+                return redirect(request.url)
         else:
             flash('Source Code Only Allowed!')
             return redirect(request.url)
